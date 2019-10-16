fe4f024eac9b217a703a54dc9e9d587f4895cfe8
David Wallwin <dawa@lubrizol.com>
2019-10-16 17:10:21 +0100

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Todos from './components/Todos'
+import logo from './logo.svg'
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
+export class App extends Component {
+  state = {
+    todos : [
+       {
+        id: 1,
+        title: "task 1",
+        completed: true
+      },
+      {
+        id: 2,
+        title: "task 2",
+        completed: false
+      },
+      {
+        id: 3,
+        title: "task 3",
+        completed: false
+      }
+    ]  
+  }
+
+  toggleComplete = id => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    });
+  };
+
+  
+  delTodo = id => {
+    console.log(`delete ${id}`);
+
+    //Axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+    // .then(res => this.setState({
+    //   todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    // }));
+  }
+
+  render() {
+    return (
+      <div className="App">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        <Todos todos={this.state.todos} toggleComplete={this.toggleComplete} delTodo={this.delTodo}/>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
+
---
+++ src/components/Todos.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import TodoItem from './TodoItem'
+import PropTypes from 'prop-types'
+
+export class Todos extends Component {
+    render() {
+            return this.props.todos.map((todo) => (
+            <TodoItem key={todo.id} todo={todo} toggleComplete={this.props.toggleComplete}  delTodo={this.props.delTodo}/>
+            )     
+            );
+    }
+}
+
+Todos.propTypes = {
+    todos : PropTypes.array.isRequired,
+    toggleComplete: PropTypes.func.isRequired,
+}
+
+export default Todos
